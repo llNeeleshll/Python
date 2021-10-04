c18766261e50b0d9093c2668783847c5fb06af65
Neelesh <neelesh.vishwakarma235@gmail.com>
2021-10-04 16:36:18 +0530

    String operations

---
+++ section_3/challange_1.py
@@ -0,0 +1,12 @@
+text = "abcdefghijklmnopqrstuvwxyz"
+
+print(text[16:13:-1])
+
+print(text[4::-1])
+
+print(text[:-9:-1])
+
+another_text = ""
+
+another_text[:1] # get the first letter, but here will give empty string.
+another_text[0] # get the first letter, but here will break the code.
---
+++ section_3/string_play.py
@@ -0,0 +1,13 @@
+text = "This is awesome."
+
+# Getting the substring
+print(text[8:])
+print(text[0:4])
+
+
+# text[start:end:step]
+print(text[0:14:2])
+print(text[0::2])
+
+# Reversing the string
+print(text[::-1])
